af0b3f12ca5e6e6add1feff395cf080aa8d46b25
asa-cording <asahiwork.cording@gmail.com>
2025-09-05 17:39:26 +0900

    rename function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { arrayOperations } from "./vanilla_js/array.js";
-import { throwError } from "./vanilla_js/throwException.js";
+import { throwException } from "./vanilla_js/throwException.js";
 
 // arrayOperations();
 
 //例外処理
 const exceptionHandler = () => {
     try {
-        throwError();//エラー発生➡catchへ
+        throwException();//エラー発生➡catchへ
     } catch (err) {
         console.error(err);
     } finally {
--- vanilla_js/throwException.js
+++ vanilla_js/throwException.js
@@ -1,6 +1,6 @@
 //例外を投げる関数
 
-export const throwError = () => {
+export const throwException = () => {
     //thwow文：意図的に例外（エラー）を発生させる
     throw new Error("エラー発生")
 }
