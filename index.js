f35befbe64214c2337e9297bd1df42efab89041f
asa-cording <asahiwork.cording@gmail.com>
2025-09-05 15:56:09 +0900

    arrayコメントアウト

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import { arrayOperations } from "./vanilla_js/array.js";
+import { throwError } from "./vanilla_js/throwException.js";
 
-arrayOperations();
+// arrayOperations();
+
+//例外処理
+const executeHandler = () => {
+    try {
+        throwError();//エラー発生➡catchへ
+    } catch (err) {
+        console.error(err);
+    } finally {
+        //エラーを捕まえても動作させたい処理があればここに書く
+    }
+};
+executeHandler();
