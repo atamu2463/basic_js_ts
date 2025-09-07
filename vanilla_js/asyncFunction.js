fed72ef3662bfe3367a7dabc5473f90d36a08468
asa-cording <asahiwork.cording@gmail.com>
2025-09-07 20:45:09 +0900

    update

--- vanilla_js/asyncFunction.js
+++ vanilla_js/asyncFunction.js
@@ -8,3 +8,21 @@ const calculateSum = new Promise((resolve) => {
 
 console.log(calculateSum); //resultのpromiseオブジェクトを返す
 console.log(await calculateSum); //promiseの中身＝calculateSum()の戻り値を返す
+
+//promiseチェーン（then/catch）を使う方法
+const promiseChain = () => {
+    calculateSum
+    //promiseがfulfilled➡thenへ
+        .then((res) => {
+            console.log(res); 
+            return res * 2; 
+        })
+        .then((res) => {
+            console.log(res); 
+        })
+    //promiseがrejected➡catchへ
+        .catch((err) => {
+            console.error(err); 
+        });
+};
+promiseChain();
