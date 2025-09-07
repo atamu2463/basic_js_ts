f285c2210fe60c0b8ca96342bb15020a7cc1e9e7
asa-cording <asahiwork.cording@gmail.com>
2025-09-07 18:57:27 +0900

    add asyncfunction.js

--- vanilla_js/asyncFunction.js
+++ vanilla_js/asyncFunction.js
@@ -1,1 +1,10 @@
 // 非同期処理, promise, async/await
+
+//promiseオブジェクトを利用する方法
+const calculateSum = new Promise((resolve) => {
+    const result = 1 + 1; 
+    resolve(result);  // promiseがfulfilledになる
+});
+
+console.log(calculateSum); //resultのpromiseオブジェクトを返す
+console.log(await calculateSum); //promiseの中身＝calculateSum()の戻り値を返す
